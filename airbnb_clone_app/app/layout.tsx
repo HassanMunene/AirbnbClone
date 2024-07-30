675635b759a8705c13a301ba9b60f5e82fa1d490
HassanMunene <awanzihassan@gmail.com>
2024-07-30 15:54:28 +0300

    setting up the application environment

---
+++ airbnb_clone_app/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+const font = Nunito({subsets: ["latin"]});
+
+export const metadata: Metadata = {
+  title: "AirBnB",
+  description: "AirBnB clone",
+};
+
+export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
