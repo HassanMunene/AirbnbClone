c56c2db4f684ac61bc3011f8f7f97ecb48ea98e0
HassanMunene <awanzihassan@gmail.com>
2024-09-05 13:39:46 +0300

    completed the Amenities section of create Listing Modal

--- airbnb/src/components/modals/CreateListingModal.jsx
+++ airbnb/src/components/modals/CreateListingModal.jsx
@@ -11,6 +11,7 @@ import CategoryInputElement from "../common/CategoryInputElement";
 import { useForm } from "react-hook-form";
 import SelectCountryDropdown from "../common/SelectCountryDropdown";
 import dynamic from "next/dynamic";
+import AmenitiesCounter from "../common/AmenitiesCounter";
 
 
 const STEPS = {
@@ -42,9 +43,12 @@ const CreateListingModal = () => {
     //keep track of the category field
     const category = watch('category');
     const locationValue = watch('locationValue');
+    const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
 
-    //const allValues = watch();
-    //console.log(allValues)
+    const allValues = watch();
+    console.log(allValues)
 
     //update and validate the value of a specific form field
     const handleCategoryChange = (categoryLabel) => {
@@ -84,32 +88,36 @@ const CreateListingModal = () => {
         return 'Back';
     }, [step]);
 
-    //body content when we are in step 0 choosing category
+    //declare an empty body content that we will overwrite depending on the step we are in
     let bodyContent = (
-        <div className="flex flex-col gap-8">
-            <div className="text-start">
-                <div className="font-bold text-2xl">Which of these best describes your place</div>
-                <div className="font-medium text-neutral-500 mt-2">Pick a category</div>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
-                {categories.map((item) => (
-                    <div key={item.label} className="col-span-1">
-                        <CategoryInputElement 
-                            onCategorySelect={handleCategoryChange}
-                            label={item.label}
-                            icon={item.icon}
-                            selected={category === item.label}
-                        />
-                    </div>
-                ))}
-            </div>
-        </div>
+        <div></div>
     )
+    //body content when we are in step 0 choosing category
+    if (step === STEPS.CATEGORY) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <div className="text-start">
+                    <div className="font-bold text-2xl">Which of these best describes your place</div>
+                    <div className="font-medium text-neutral-500 mt-2">Pick a category</div>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+                    {categories.map((item) => (
+                        <div key={item.label} className="col-span-1">
+                            <CategoryInputElement 
+                                onCategorySelect={handleCategoryChange}
+                                label={item.label}
+                                icon={item.icon}
+                                selected={category === item.label}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )
+    }
 
     //dynamically import the Map component reasons for this are written in my Airbnb clone book
     const Map = useMemo(() => dynamic(() => import('../map/Map'), {ssr: false}), [locationValue])
-
-
     //body content when we are in step 1 choosing location
     if (step === STEPS.LOCATION) {
         bodyContent = (
@@ -127,6 +135,35 @@ const CreateListingModal = () => {
         )
     }
 
+    //body content when we are in step 2 adding more Information
+    if (step === STEPS.INFO) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <div className="text-start">
+                    <div className="font-bold text-2xl">Share some basics about your place</div>
+                    <div className="font-medium text-neutral-500 mt-2">What amenities do you have?</div>
+                </div>
+                <AmenitiesCounter 
+                    title="Guests" 
+                    subtitle="How many guests do you allow?"
+                    value={guestCount}
+                    onChange={(value) => setValue('guestCount', value)}
+                />
+                <AmenitiesCounter 
+                    title="Rooms" 
+                    subtitle="How many rooms do you have?"
+                    value={roomCount}
+                    onChange={(value) => setValue('roomCount', value)}
+                />
+                <AmenitiesCounter 
+                    title="Bathrooms" 
+                    subtitle="How many bathrooms do you have?"
+                    value={bathroomCount}
+                    onChange={(value) => setValue('bathroomCount', value)}
+                />
+            </div>
+        )
+    }
 
 
     return (
